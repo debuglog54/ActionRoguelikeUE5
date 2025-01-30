c6d73735c1abc42b6cb241b2738ad30e8f464bbf
debuglog54 <109488398+debuglog54@users.noreply.github.com>
2025-01-30 02:04:17 -0800

    InteractInterface & InteractComponent

--- ActionRoguelike/Source/ActionRoguelike/Private/SCharacter.cpp
+++ ActionRoguelike/Source/ActionRoguelike/Private/SCharacter.cpp
@@ -6,98 +6,110 @@
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "DrawDebugHelpers.h"
+#include "SInteractionComponent.h"
 
 
 // Sets default values
 ASCharacter::ASCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
-	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringComponent");
-	SpringArmComp->bUsePawnControlRotation = true;
-	SpringArmComp->SetupAttachment(RootComponent);
-	
-	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
-	CameraComp->SetupAttachment(SpringArmComp);
+    SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringComponent");
+    SpringArmComp->bUsePawnControlRotation = true;
+    SpringArmComp->SetupAttachment(RootComponent);
 
-	GetCharacterMovement()->bOrientRotationToMovement = true;
+    CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+    CameraComp->SetupAttachment(SpringArmComp);
 
-	bUseControllerRotationYaw = false;
+    InteractionComp = CreateDefaultSubobject<USInteractionComponent>("InteractionComponent");
+
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+
+    bUseControllerRotationYaw = false;
 }
 
 // Called when the game starts or when spawned
 void ASCharacter::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
 }
 
 void ASCharacter::MoveForward(float value)
 {
-	FRotator ControlRot = GetControlRotation();
-	ControlRot.Pitch = 0;
-	ControlRot.Roll = 0;
+    FRotator ControlRot = GetControlRotation();
+    ControlRot.Pitch = 0;
+    ControlRot.Roll = 0;
 
-	AddMovementInput(ControlRot.Vector(), value);
+    AddMovementInput(ControlRot.Vector(), value);
 }
 
 void ASCharacter::MoveRight(float value)
 {
-	FRotator ControlRot = GetControlRotation();
-	ControlRot.Pitch = 0;
-	ControlRot.Roll = 0;
-	FVector RightVector = FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y);
-	
-	AddMovementInput(RightVector, value);
+    FRotator ControlRot = GetControlRotation();
+    ControlRot.Pitch = 0;
+    ControlRot.Roll = 0;
+    FVector RightVector = FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y);
+
+    AddMovementInput(RightVector, value);
 }
 
 void ASCharacter::PrimaryAttack()
 {
-	FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
-	FTransform SpawnTM = FTransform(GetControlRotation(), HandLocation);
-	FActorSpawnParameters SpawnParams;
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+    FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
+    FTransform SpawnTM = FTransform(GetControlRotation(), HandLocation);
+    FActorSpawnParameters SpawnParams;
+    SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
-	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
+    GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
+}
+
+void ASCharacter::PrimaryInteract()
+{
+    if (InteractionComp)
+    {
+        InteractionComp->PrimaryInteract();
+    }
 }
 
 // Called every frame
 void ASCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 
-	// -- Rotation Visualization -- //
-	const float DrawScale = 100.0f;
-	const float Thickness = 5.0f;
+    // -- Rotation Visualization -- //
+    const float DrawScale = 100.0f;
+    const float Thickness = 5.0f;
 
-	FVector LineStart = GetActorLocation();
-	// Offset to the right of pawn
-	LineStart += GetActorRightVector() * 100.0f;
-	// Set line end in direction of the actor's forward
-	FVector ActorDirection_LineEnd = LineStart + (GetActorForwardVector() * 100.0f);
-	// Draw Actor's Direction
-	DrawDebugDirectionalArrow(GetWorld(), LineStart, ActorDirection_LineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
+    FVector LineStart = GetActorLocation();
+    // Offset to the right of pawn
+    LineStart += GetActorRightVector() * 100.0f;
+    // Set line end in direction of the actor's forward
+    FVector ActorDirection_LineEnd = LineStart + (GetActorForwardVector() * 100.0f);
+    // Draw Actor's Direction
+    DrawDebugDirectionalArrow(GetWorld(), LineStart, ActorDirection_LineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
 
-	FVector ControllerDirection_LineEnd = LineStart + (GetControlRotation().Vector() * 100.0f);
-	// Draw 'Controller' Rotation ('PlayerController' that 'possessed' this character)
-	DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerDirection_LineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);
+    FVector ControllerDirection_LineEnd = LineStart + (GetControlRotation().Vector() * 100.0f);
+    // Draw 'Controller' Rotation ('PlayerController' that 'possessed' this character)
+    DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerDirection_LineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);
 }
 
 // Called to bind functionality to input
 void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
 
 
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+    PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 
-	PlayerInputComponent->BindAction("PrimaryAttack", IE_Pressed, this, &ASCharacter::PrimaryAttack);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASCharacter::Jump);
+    PlayerInputComponent->BindAction("PrimaryAttack", IE_Pressed, this, &ASCharacter::PrimaryAttack);
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASCharacter::Jump);
 
+    PlayerInputComponent->BindAction("PrimaryInteract", IE_Pressed, this, &ASCharacter::PrimaryInteract);
 }
 
